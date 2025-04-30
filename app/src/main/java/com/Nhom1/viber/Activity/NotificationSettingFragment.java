a068a79ab6eeface0329fb6c74ec7ddce6d3ed88
NHDHuy2004 <2212381@dlu.edu.vn>
2025-04-30 15:31:13 +0700

    cap nhat

--- app/src/main/java/com/Nhom1/viber/Activity/NotificationSettingFragment.java
+++ app/src/main/java/com/Nhom1/viber/Activity/NotificationSettingFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +19,19 @@ public class NotificationSettingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notification_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_notification_setting, container, false);
+
+        Switch notificationSwitch = view.findViewById(R.id.switch_notification);
+        notificationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // Bật thông báo
+                Toast.makeText(getContext(), "Thông báo đã được bật", Toast.LENGTH_SHORT).show();
+            } else {
+                // Tắt thông báo
+                Toast.makeText(getContext(), "Thông báo đã được tắt", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
